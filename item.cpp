b41bc8cedb69a9b573a8969103e7de28c9feef02
Livia Pimentel <livia_macedo89@yahoo.com.br>
2025-04-06 03:13:49 -0300

    Add getters and setters

--- item.cpp
+++ item.cpp
@@ -4,3 +4,29 @@
 // Constructor 
 Item::Item(std::string name, int quantity, double price); // Declares the constructor of the class Item
     : name(name), quantity(quantity), price(price){} // Initialization list
+
+// Getters
+std::string Item::getName() const {
+    return name
+}
+
+int Item::getQuantity() const {
+    return quantity;
+}
+
+double Item::getPrice() const {
+    return price;
+}
+
+// Setters
+void Item::setName(std::string name) {
+    this -> name = name;
+}
+
+void Item::setQuantity(int quantity) {
+    this -> quantity = quantity;
+}
+
+void Item::setPrice(double price) {
+    this -> price = price;
+}
