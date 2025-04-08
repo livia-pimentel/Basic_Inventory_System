75f2d1671cccc6dd6649c63e03cebcc8759b6420
Livia Pimentel <livia_macedo89@yahoo.com.br>
2025-04-08 13:39:10 -0300

    Test readFile and displayInventory

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
-#include "item.h"
+#include "inventory.h"
 #include <iostream>
 
 int main() {
+    Inventory inventory;
 
+    // Loads the file data
+    inventory.readFile("inventory.txt");
+
+    // Displays inventory list
+    std::cout << "Inventory List:\n";
+    inventory.displayInventory();
+
+    std::cout << "\nEnd of Inventory List\n";
+
+    return 0;
 }
